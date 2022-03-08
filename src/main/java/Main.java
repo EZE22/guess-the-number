02d6858ac79e15121a2c0ce41f613b8ce25b0a7b
Adam <everett.ingram22@gmail.com>
2022-03-08 14:11:15 -0500

    Started Start Game Method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
-public class Main {
-   public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+   //Declare a Scanner object
+   private static final Scanner scanner = new Scanner(System.in);
+
+   public static void main(String[] args) {
+      //Start Game
+      startGame();
+
+      //Finish Game
+      System.out.println("\nThank you for plaing!");
+   }
+
+   //Putting methods here
+   public static void startGame() {
+      System.out.println(getUserName());
+      
+      //Game Logic
+
+
+   }
+
+   //Getting user name
+   public static String getUserName() {
+      System.out.println("Please enter your name: ");
+      String name = scanner.next();
+      return "Welcome " + name + " to the guess the number game!";
    }
 }
